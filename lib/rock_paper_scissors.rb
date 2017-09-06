82dbb7e83fca93fd4237d1e78e47781df975a095
Debbie Michel and Snow Vilay <snow@>
2017-09-06 11:50:31 -0700

    Pass 9 specs

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -10,27 +10,27 @@ end
 
 
 public def wins(p1move, p2move)
+
   if(p1move === "rock" && p2move === "scissors")
     "player 1 wins"
   elsif(p1move === "scissors" && p2move === "paper")
     "player 1 wins"
-  # if(self.move === "rock")
-  #   true
-  # elsif(Player.Move === "paper")
-  #   true
-  # elsif(Player.Move === "scissors")
+  elsif(p1move === "paper" && p2move === "rock")
+    "player 1 wins"
+  elsif(p2move === "rock" && p1move === "scissors")
+      "player 2 wins"
+  elsif(p2move === "scissors" && p1move === "paper")
+    "player 2 wins"
+  elsif(p2move === "paper" && p1move === "rock")
+    "player 2 wins"
+  elsif(p1move === "rock" && p2move === "rock")
+    "It's a tie"
+  elsif(p1move === "scissors" && p2move === "scissors")
+    "It's a tie"
+  elsif(p1move === "paper" && p2move === "paper")
+    "It's a tie"
 
-  # if("rock" > "scissors")
-  #   true
-  # elsif("paper" > "rock")
-  #   true
-  # elsif("scissors" > "paper")
-  #   true
-  # elsif("paper" === "paper")
-  #   true
-  # elsif("scissors" === "scissors")
-  #   true
-  # elsif("rock" === "rock")
-  #   true
   end
+
+
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -3,14 +3,50 @@ require('rspec')
 require('pry')
 
 describe('#rock_paper_scissors')do
-  it("return player1 to beat player2")do
-  player1 = Player.new("snow", "rock")
-  player2 = Player.new("debbie", "scissors")
-  expect(wins(player1.move, player2.move)).to(eq("player 1 wins"))
+  it("return player1 to beat player2, if player1 has rock and player2 has scissors")do
+    player1 = Player.new("snow", "rock")
+    player2 = Player.new("debbie", "scissors")
+    expect(wins(player1.move, player2.move)).to(eq("player 1 wins"))
   end
-  it("return player1 to beat player2")do
+  it("return player1 to beat player2, if player1 has scissors and player2 has paper")do
     player1 = Player.new("snow", "scissors")
     player2 = Player.new("debbie", "paper")
     expect(wins(player1.move, player2.move)).to(eq("player 1 wins"))
   end
+  it("return player1 to beat player2, if player1 has paper and player2 has rock")do
+    player1 = Player.new("snow", "paper")
+    player2 = Player.new("debbie", "rock")
+    expect(wins(player1.move, player2.move)).to(eq("player 1 wins"))
+  end
+  it("return player2 to beat player1, if player2 has rock and player1 has scissors")do
+    player2 = Player.new("snow", "rock")
+    player1 = Player.new("debbie", "scissors")
+    expect(wins(player1.move, player2.move)).to(eq("player 2 wins"))
+  end
+  it("return player2 to beat player1, if player2 has scissors and player1 has paper")do
+    player2 = Player.new("snow", "scissors")
+    player1 = Player.new("debbie", "paper")
+    expect(wins(player1.move, player2.move)).to(eq("player 2 wins"))
+  end
+  it("return player2 to beat player1, if player2 has paper and player1 has rock")do
+    player2 = Player.new("snow", "paper")
+    player1 = Player.new("debbie", "rock")
+    expect(wins(player1.move, player2.move)).to(eq("player 2 wins"))
+  end
+  it("returns a tie when both players choose rock")do
+    player2 = Player.new("snow", "rock")
+    player1 = Player.new("debbie", "rock")
+    expect(wins(player1.move, player2.move)).to(eq("It's a tie"))
+  end
+  it("returns a tie when both players choose scissors")do
+    player2 = Player.new("snow", "scissors")
+    player1 = Player.new("debbie", "scissors")
+    expect(wins(player1.move, player2.move)).to(eq("It's a tie"))
+  end
+  it("returns a tie when both players choose paper")do
+    player2 = Player.new("snow", "paper")
+    player1 = Player.new("debbie", "paper")
+    expect(wins(player1.move, player2.move)).to(eq("It's a tie"))
+  end
+
 end
